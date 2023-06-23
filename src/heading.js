e709a4bbb8b3d97123066c934d8a8984f29a26e4
chirag <chirag42001@gmail.com>
2023-06-23 13:31:34 +0530

    background color chaged

--- src/heading.js
+++ src/heading.js
@@ -3,6 +3,6 @@ import './heading.css';
 export default function Header({city,name,state})
 {
     return(
-        <div id="heading"> <span id="name">{name}</span>  <br /> <span id="place">{city}, {state}</span> </div>
+        <div id="heading"> <span id="name">{name}</span>  <br /> <span id="place">{city===''?null:city+','} {state}</span> </div>
     );
 }
